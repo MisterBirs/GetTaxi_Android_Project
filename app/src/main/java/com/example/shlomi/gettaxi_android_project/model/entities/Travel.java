332698d85dec561b80bf09a31b2d2fc61d593897
Shlomi Birs <shlomibirs@gmail.com>
2018-11-12 15:25:17 +0200

    Create Travel Class and TravelStatus enum.

---
+++ app/src/main/java/com/example/shlomi/gettaxi_android_project/model/entities/Travel.java
@@ -0,0 +1,14 @@
+package com.example.shlomi.gettaxi_android_project.model.entities;
+
+import java.util.Date;
+
+public class Travel {
+    protected TravelStatus status;
+    protected String sourceLocation;
+    protected String destinationLocation;
+    protected Date startTime;
+    protected Date endTime;
+    protected String customerName;
+    protected String customerPhone;
+    protected String customerEmail;
+}
---
+++ app/src/main/java/com/example/shlomi/gettaxi_android_project/model/entities/TravelStatus.java
@@ -0,0 +1,7 @@
+package com.example.shlomi.gettaxi_android_project.model.entities;
+
+public enum TravelStatus {
+    READY,
+    TREATED,
+    OVER
+}
